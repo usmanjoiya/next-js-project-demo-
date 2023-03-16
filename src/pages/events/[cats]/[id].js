3f8bdf7fe3197b026a4bdbecd1bd75118af9d8d4
Usman Akbar <45451919+usmanjoiya@users.noreply.github.com>
2023-03-16 18:48:12 +0500

    next app

---
+++ src/pages/events/[cats]/[id].js
@@ -0,0 +1,11 @@
+const EventPage = () => {
+// this is a dynamic page that can exist after any events page
+//this helps to dynamically create more than 1 dyanmic page that can 
+// be used as a default
+    return (
+        
+        <h1>Our Single Event</h1>
+        
+    )
+}
+export default EventPage;
---
+++ src/pages/events/index.js
@@ -0,0 +1,17 @@
+const EventsPage = () => {
+
+    return (
+        <div>
+        
+        <h1> Events Page </h1>
+<div>
+<a href=''> <img/> <h2>Events In London</h2> </a>
+<a href=''> <img/> <h2>Events In San Fran</h2> </a>
+<a href=''> <img/> <h2>Events In Barcelona</h2> </a>
+</div>
+        </div>
+    )
+}
+
+export default EventsPage;
+
